af1d4524860eeb65a9591358271b478af0110797
Ngo T Luan <luan.ngot@nipponexpress.com>
2023-01-06 15:42:26 +0700

    fix v3

--- index.js
+++ index.js
@@ -1,36 +1,60 @@
 const express = require('express');
 const app = express();
 const port = 3000 || process.env.PORT;
-const bodyParser = require('body-parser')
-    const admin = require('firebase-admin');
-    const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
+const bodyParser = require('body-parser');
+const cancelStatus = "CANCELLED";
+const admin = require('firebase-admin');
+const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
 
-    const serviceAccount = require("./sercet.json");
+const serviceAccount = require("./sercet.json");
 
-    admin.initializeApp({
-        credential : admin.credential.cert(serviceAccount)
-    })
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+})
 
-    const db = getFirestore();
+const db = getFirestore();
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended : true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 
-app.post('/update_ahamove_status',(req,res)=>{
-    
+app.post('/update_ahamove_status', (req, res) => {
+
     const body = req.body;
-    console.log(body);
-    
-    const collection = db.collection('test');
-    collection.doc().set(body,{merge : true});
 
-    res.json({sucessful : true,msg:'Ahamove webhook!!'});
+    const id = body._id;
+    const status = body.status;
+    const trackingNumber = body.path[1].tracking_number;
+    const phone = body.supplier_id;
+    const supplierName = body.supplier_name;
+    const cancelComment = body.cancel_comment;
+    const cancelByUser = body.cancel_by_user;
+
+    const obj = {
+        id : id,
+        status : status,
+        trackingNumber : trackingNumber,
+        phone : phone,
+        supplierName : supplierName,
+        cancelComment : cancelComment,
+        cancelByUser : cancelByUser,
+        created : new Date()
+    }
+
+    const collection = db.collection('test');
+    
+    if(!trackingNumber.trim()){
+         collection.doc().set(obj,{merge : true});
+    }else{
+         collection.doc(obj.trackingNumber).set(obj,{merge:true});
+    }
+
+    res.json({ sucessful: true, msg: 'Ahamove webhook!!' });
 });
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send("Hello everyone!!!");
 })
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log("Server is running at " + port);
 })
